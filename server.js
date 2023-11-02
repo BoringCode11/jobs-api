a75eb061dc429e81bdf6c18c3bc2345902e64acf
lesed1 <lesedi.xys12@gmail.com>
2023-11-02 15:30:20 +0200

    initial commit

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ require('express-async-errors')
 // extra security packages
 const helmet = require('helmet')
 const cors = require('cors')
-const xss = require('xss-clean')
-const rateLimiter = require('express-rate-limit')
 
 const express = require('express')
 const app = express()
@@ -22,16 +20,9 @@ const notFound = require('./middleware/not-found')
 const errorHandler = require('./middleware/error-handler')
 const authMiddleware = require('./middleware/auth')
 
-
-app.set('trust proxy', 1)
-app.use(rateLimiter({
-  windowMs: 15 * 60 * 1000,
-  max: 100
-}))
 app.use(express.json())
 app.use(helmet())
 app.use(cors())
-app.use(xss())
 
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/jobs', authMiddleware, jobsRouter)
